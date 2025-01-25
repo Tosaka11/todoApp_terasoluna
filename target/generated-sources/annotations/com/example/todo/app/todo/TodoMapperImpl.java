825a3dd26d8ce5e036d738411b4742d5cc98d990
Ken <ken@mail.com>
2025-01-25 13:40:22 +0900

    first commit

---
+++ target/generated-sources/annotations/com/example/todo/app/todo/TodoMapperImpl.java
@@ -0,0 +1,28 @@
+package com.example.todo.app.todo;
+
+import com.example.todo.domain.model.Todo;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-25T13:29:57+0900",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.11 (Eclipse Adoptium)"
+)
+@Component
+public class TodoMapperImpl implements TodoMapper {
+
+    @Override
+    public Todo map(TodoForm form) {
+        if ( form == null ) {
+            return null;
+        }
+
+        Todo todo = new Todo();
+
+        todo.setTodoId( form.getTodoId() );
+        todo.setTodoTitle( form.getTodoTitle() );
+
+        return todo;
+    }
+}
